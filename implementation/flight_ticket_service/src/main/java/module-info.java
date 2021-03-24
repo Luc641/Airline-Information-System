7a13084e8433c7263ce02100905678407f33e780
patrickvanleipsig <p.vanleipsig@student.fontys.nl>
2021-03-24 14:05:37 +0100

    delete flight_ticket_service folder

--- implementation/flight_ticket_service/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.group_twelve.flight_ticket_service {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.group_twelve.flight_ticket_service to javafx.fxml;
-    exports com.group_twelve.flight_ticket_service;
-}
