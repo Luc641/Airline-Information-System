e7e6aca710bc426f530c74e365fb958d18a6a939
lucle <l.lehmkuhl@student.fontys.nl>
2021-03-30 20:08:14 +0200

    Making the Application work.

---
+++ implementation/GUI/src/main/java/com/group_twelve/gui/registerFlight.java
@@ -0,0 +1,21 @@
+package com.group_twelve.gui;
+
+import javafx.fxml.FXML;
+
+import java.util.ArrayList;
+
+public class registerFlight {
+
+    @FXML
+    public boolean storeFlight() {
+        getFlightData();
+        return true;
+    }
+
+    public void storeInformation() {
+
+    }
+
+    public void getFlightData() {
+    }
+}
