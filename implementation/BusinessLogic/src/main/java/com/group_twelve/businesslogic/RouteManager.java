c93655c068e52dfe8fde049781ed67c65bffe5b5
t-mattern <70884134+t-mattern@users.noreply.github.com>
2021-05-04 13:17:42 +0200

    Changed structure

--- implementation/BusinessLogic/src/main/java/com/group_twelve/businesslogic/RouteManager.java
+++ implementation/BusinessLogic/src/main/java/com/group_twelve/businesslogic/RouteManager.java
@@ -1,52 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.group_twelve.businesslogic;
 
 import com.group_twelve.entities.Route;
-import java.util.ArrayList;
-import java.util.List;
+import com.group_twelve.persistence.RoutePersistence;
 
 /**
  *
  * @author Timo Mattern (t.mattern@student.fontys.nl, github: @t-mattern)
  */
+
 public class RouteManager implements Manager<Route>{
+
+    private final RoutePersistence persistence;
     
-    List<Route> routes;
-    
-    public RouteManager() {
-        this.routes = new ArrayList<>();
+    public RouteManager(RoutePersistence persistence) {
+        this.persistence = persistence;
     }
     
-    public RouteManager(List<Route> routes) {
-        this.routes = routes;
-    }
-    
-    public static Route create() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    public static Route create(String[] args) {
+    public static Route createRouteFromArgs(String[] args) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     public void add(Route route) {
-        routes.add(route);
-    }
-    
-    public List<Route> getAll() {
-        return routes;
-    }
-    
-    public void clear() {
-        routes.clear();
-    }
-    
-    public void setAll(List<Route> routes) {
-        this.routes = routes;
+        //validate
+        //persistence.add(route)
     }
     
 }
