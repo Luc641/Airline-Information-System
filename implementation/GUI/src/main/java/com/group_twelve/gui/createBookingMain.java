64ceadd0e9fcedd74cdc8f8eb2781d6a2a21559b
patrickvanleipsig <p.vanleipsig@student.fontys.nl>
2021-05-18 11:37:51 +0200

    Made progress on controller.

--- implementation/GUI/src/main/java/com/group_twelve/gui/createBookingMain.java
+++ implementation/GUI/src/main/java/com/group_twelve/gui/createBookingMain.java
@@ -1,7 +1,12 @@
 package com.group_twelve.gui;
 
 import com.group_twelve.businesslogic.AirportManager;
+import com.group_twelve.businesslogic.FlightManager;
+import com.group_twelve.businesslogic.RouteManager;
 import com.group_twelve.entities.Airport;
+import com.group_twelve.entities.Route;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
@@ -9,6 +14,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class createBookingMain {
@@ -17,7 +23,7 @@ public class createBookingMain {
     @FXML
     private TextField txtDepatureAirport;
     @FXML
-    private ListView listAvailableFlights;
+    private ListView listAvailableRoutes;
     @FXML
     private TextField txtArrivalAirport;
     @FXML
@@ -43,9 +49,22 @@ public class createBookingMain {
     public void searchFlightRoutes(){
         // Init managers
         AirportManager apm = (AirportManager) GUIApp.getBusinessLogicAPI().getManager(Airport.class);
+        RouteManager rm = (RouteManager) GUIApp.getBusinessLogicAPI().getManager(Route.class);
 
-        
+        // Get values from UI
+        String depAirport = txtDepatureAirport.getText();
+        String arAirport = txtArrivalAirport.getText();
 
+        // Get flight route(s) that have matching airports
+//        List<Route> routes = rm.getRouteBasesOnAirports(apm.getAirportId(depAirport), apm.getAirportId(arAirport));
+        List<Route> routes = rm.getRouteBasesOnAirports(apm.getAirportId("Berlin"), apm.getAirportId("New York"));
+
+
+        // TODO: make listview work
+        ObservableList<String> test = FXCollections.observableArrayList("ee", "egeg", "gege");
+        listAvailableRoutes = new ListView<>(test);
+        listAvailableRoutes.setItems(test);
+        System.out.println(test);
     }
 
 }
