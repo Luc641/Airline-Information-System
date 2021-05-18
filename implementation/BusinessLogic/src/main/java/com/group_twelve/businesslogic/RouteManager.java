6700a12e65013644d15f17773de02b0a03dcd0e2
patrickvanleipsig <p.vanleipsig@student.fontys.nl>
2021-05-18 11:37:38 +0200

    Edited route entity + added get route functionality

--- implementation/BusinessLogic/src/main/java/com/group_twelve/businesslogic/RouteManager.java
+++ implementation/BusinessLogic/src/main/java/com/group_twelve/businesslogic/RouteManager.java
@@ -5,7 +5,10 @@
  */
 package com.group_twelve.businesslogic;
 
+import com.group_twelve.entities.Airport;
 import com.group_twelve.entities.Route;
+import com.group_twelve.persistence.RoutePersistence;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,38 +18,60 @@ import java.util.List;
  */
 public class RouteManager implements Manager<Route>{
     
-    List<Route> routes;
+    RoutePersistence persistence;
     
-    public RouteManager() {
-        this.routes = new ArrayList<>();
+    public RouteManager(RoutePersistence rp) {
+        this.persistence = rp;
     }
     
-    public RouteManager(List<Route> routes) {
-        this.routes = routes;
-    }
+//    public RouteManager(List<Route> routes) {
+//        this.routes = routes;
+//    }
     
     public static Route create() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     public static Route create(String[] args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new Route(Integer.valueOf(args[0]), args[1], Integer.valueOf(args[2]));
     }
     
     public void add(Route route) {
-        routes.add(route);
+
     }
     
     public List<Route> getAll() {
-        return routes;
+        try{
+//            List<Route> flights = persistence.load();
+            return persistence.load();
+        }catch(Exception e){
+            System.out.println("Failed to get all flight routes");
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    /**
+     *
+     * @param aID = arrivalAirportID
+     * @param dID = departureAirportID
+     * @return list of found routes that have the arrival- and departureairport in common.
+     */
+    public List<Route> getRouteBasesOnAirports(int aID, int dID){
+        try{
+            return persistence.getRoutesBasedOnAirports(aID, dID);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
     }
     
     public void clear() {
-        routes.clear();
+
     }
     
     public void setAll(List<Route> routes) {
-        this.routes = routes;
+//        this.routes = routes;
     }
     
 }
